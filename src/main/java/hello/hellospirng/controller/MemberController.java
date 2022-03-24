063b65849561af615c9bfb0aa11e8e6561892d39
kimwg97 <kimwg97@gmail.com>
2022-03-24 19:49:04 +0900

    Read Member List & Create Member

--- src/main/java/hello/hellospirng/controller/MemberController.java
+++ src/main/java/hello/hellospirng/controller/MemberController.java
@@ -1,8 +1,15 @@
 package hello.hellospirng.controller;
 
+import hello.hellospirng.domain.Member;
 import hello.hellospirng.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.method.annotation.ViewNameMethodReturnValueHandler;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -11,6 +18,27 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String creatForm(){
+        return "members/creatMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm){
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.Join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String List(Model model){
+        List<Member> members = memberService.findMember();
+        model.addAttribute("members", members);
+        return "members/memberlist";
     }
 
 }
